6ee8762c563a747157bc16ac52e31ff75041bdb7
Ayakura Yuki <AyakuraYuki@users.noreply.github.com>
2017-05-22 17:09:14 +0800

    Add MySQL support

--- CodErator/MainForm.Designer.cs
+++ CodErator/MainForm.Designer.cs
@@ -29,21 +29,39 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.groupBoxConnectInfo = new System.Windows.Forms.GroupBox();
             this.SuspendLayout();
+            // 
+            // groupBoxConnectInfo
+            // 
+            this.groupBoxConnectInfo.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxConnectInfo.Name = "groupBoxConnectInfo";
+            this.groupBoxConnectInfo.Size = new System.Drawing.Size(249, 211);
+            this.groupBoxConnectInfo.TabIndex = 0;
+            this.groupBoxConnectInfo.TabStop = false;
+            this.groupBoxConnectInfo.Text = "连接信息";
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.groupBoxConnectInfo);
+            this.Font = new System.Drawing.Font("微软雅黑", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximumSize = new System.Drawing.Size(1920, 1080);
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
             this.Name = "MainForm";
+            this.Opacity = 0.95D;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "MainForm";
+            this.Text = "CodeErator";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxConnectInfo;
     }
 }
